994b12808e28a545315d3ee9cde14b781299cef6
Takayuki Shimizukawa <takayuki.shimizukawa@beproud.jp>
2021-07-02 06:05:41 +0900

    provide `tenant_context` contextmanager

--- rls/config/middleware.py
+++ rls/config/middleware.py
@@ -1,4 +1,23 @@
 from django.db import connection
+from contextlib import contextmanager
+
+
+@contextmanager
+def tenant_context(tenant_id: int):
+    """enter tenant context with tenant_id
+    ::
+
+        # without tenant RLS
+
+        with tenant_context(tenant_id):
+            # All commands here are ran under the `tenant_id` RLS
+
+        # without tenant RLS
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(f'SET ROLE "{tenant_id}" ')
+        yield
+        cursor.execute(f'RESET ROLE')
 
 
 # set db role for RLS control. see: https://scrapbox.io/shimizukawa/Django_PG_RLS
@@ -9,8 +28,8 @@ class RlsMiddleware:
     def __call__(self, request):
         tenant_id = getattr(request.user, 'tenant_id', None)
         if tenant_id:
-            with connection.cursor() as cursor:
-                cursor.execute(f'SET ROLE "{tenant_id}" ')
-
-        response = self.get_response(request)
+            with tenant_context(tenant_id):
+                response = self.get_response(request)
+        else:  # for administration of all tenants.
+            response = self.get_response(request)
         return response
